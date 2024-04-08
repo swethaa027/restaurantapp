3960c759cd96f1007567960d119731af97432de6
swetha's mac <swethasmac@swethas-MacBook-Air.local>
2024-04-08 19:32:55 +0530

    first commit

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,45 +1,57 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { Button, TextField, Typography, } from '@mui/material'
-// import axios from 'axios';
-const Signup = () => {
-//     var[data,setData]=useState();
-//     const inputHandler =(e)=>{
-//         setData({...data,[e.target.name]:e.target.value})
-//             console.log(data);
-//     } 
-//     const submit=()=>{
-//       axios.post("http://localhost:3000/Signup",{
-//         Firstname:data.Firstname,
-//         Lastname:data.Lastname,
-//         Email:data.Email,
-//         Mobileno:data.Mobileno,
-//         Password:data.Password,
-//         Confirmpassword:data.Confirmpassword
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'; 
 
-//       })
-//       .then(()=>{
-    //     alert("new entry created successfully");
-    //   })
-    //   .catch(()=>{
-    //     alert("error saving data");
+const Signup = () => {
+    var[data,setData]=useState({});
+    const inputHandler =(e)=>{
+        setData({...data,[e.target.name]:e.target.value})
+            console.log(data);
+
+    } 
+const navigate = useNavigate(); 
+
+
+
+const submit=()=>{
+      axios.post("http://localhost:3001/Signup",{
+        Username:data.Username,
+        Email:data.Email,
+        Mobileno:data.Mobileno,
+        Password:data.Password
+        
+
+      })
+
+     
+
+      .then(()=>{
+        alert("new entry created successfully");
+        navigate("/login")
+      })
+      .catch(()=>{
+        alert("error saving data");
       
-    //   });
+      });
+
+      navigate('/Login');
+    };
   return (
     <div>
         <br /><br /><br /><br /><br />
         <Typography fontFamily='fantasy' variant='h3'  >Signup</Typography>
         <br /><br />
-        <TextField variant='outlined' color='success' label='Firstname' name='Firstname' />&nbsp;&nbsp;
-        <TextField variant='outlined' color='success' label='Lastname' name='Lastname' />
+        <TextField variant='outlined' color='success' label='Username' name='Username' onChange={inputHandler} />&nbsp;&nbsp;
+        
+        <TextField variant='outlined' color='success' label='Email' name='Email' onChange={inputHandler}/><br /><br />
+        <TextField variant='outlined' color='success' label='Mobile no' name='Mobileno' onChange={inputHandler}/>&nbsp;&nbsp;
+        
+        
+        <TextField variant='outlined' color='success' type='password' label='Password' name='Password' onChange={inputHandler}/>&nbsp;&nbsp;
         <br /><br />
-        <TextField variant='outlined' color='success' label='Email' name='Email' />&nbsp;
-        <TextField variant='outlined' color='success' label='Mobile no' name='Mobileno' />
-        <br /><br />
-        <TextField variant='outlined' color='success' label='Password' name='Password' />&nbsp;
-        <TextField variant='outlined' color='success' label='Confirm password' name='Confirmpassword' />
-        <br /><br />
-        <Button variant='contained' color='inherit' >Submit</Button>&nbsp;
-        <Button variant='contained' color='inherit'>Reset</Button>
+        <Button variant='contained' color='inherit' onClick={submit}>Submit</Button>&nbsp;
+       
     </div>
   )
 //   }
